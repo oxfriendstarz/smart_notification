345cd037d11d53ea6b8eaf1ad23fa2f649efaaa8
rlcapunpon <capunponrlc@gmail.com>
2018-05-12 01:53:26 +0800

    updated rest controller

--- buster/src/main/java/com/buster/controller/ProfileController.java
+++ buster/src/main/java/com/buster/controller/ProfileController.java
@@ -1,20 +1,34 @@
 package com.buster.controller;
 
 import com.buster.Profile;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/api/**")
 @RestController
-
+@RequestMapping("/profile")
 public class ProfileController {
-  @RequestMapping(method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
 
-  public Profile index() {
-    return new Profile("green", 10, 7);
-  }
+    @ResponseBody
+    @RequestMapping(value = "/user/{id}", method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity<?> getUser(@PathVariable("id") String id) {
+        Profile profile = new Profile("green", 10, 7);
+        return new ResponseEntity<Profile>(profile, HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/login", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<?> login(@RequestParam("username") int username,
+                                   @RequestParam("password") int password) {
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/user/register", method = RequestMethod.POST, produces = "application/json")
+    public ResponseEntity<?> register(@RequestBody Profile profile) {
+        return new ResponseEntity<>("Success", HttpStatus.OK);
+    }
+
 }
 
 
