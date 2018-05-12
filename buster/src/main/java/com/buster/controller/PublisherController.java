45b157a23a2bb6e455caf59bfe224add69f36c6e
rlcapunpon <capunponrlc@gmail.com>
2018-05-12 11:11:26 +0800

    updated structure of report to not require id in request

--- buster/src/main/java/com/buster/controller/PublisherController.java
+++ buster/src/main/java/com/buster/controller/PublisherController.java
@@ -1,13 +1,7 @@
 package com.buster.controller;
 
-
-import com.buster.Profile;
 import com.buster.models.*;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.*;
 import services.BusterMessageProducer;
 import services.ReportAssuranceService;
 
